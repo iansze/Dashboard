802d011966e8e82ec4fbdf29a48b366d1bc5d61e
Ian Sze <iansze1124@gmail.com>
2024-01-11 16:25:57 -0500

    test

--- backend/frontend/src/pages/orders/Orders.tsx
+++
@@ -1,8 +0,0 @@
-import { requests } from "../../utils/axios";
-import DataTable from "../../components/dataTable/DataTable";
-
-const Orders = () => {
-  return <DataTable query={requests.fetchOrders} tag={"fetchOrders"} />;
-};
-
-export default Orders;
--- backend/frontend/src/pages/users/Users.tsx
+++
@@ -1,9 +0,0 @@
-import { requests } from "../../utils/axios";
-
-import DataTable from "../../components/dataTable/DataTable";
-
-const Users = () => {
-  return <DataTable query={requests.fetchUsers} tag={"fetchUsers"} />;
-};
-
-export default Users;
